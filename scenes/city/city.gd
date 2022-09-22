4b82bf4df37aae273e4a575cbbc3d972ac8c8fb3
Alon Ran <newdefectus@gmail.com>
2022-09-22 12:23:03 +0300

    Add background-layer pillars to levels

--- scenes/city/city.gd
+++ scenes/city/city.gd
@@ -24,7 +24,7 @@ func _physics_process(_delta):
 		_stood_on_car = false
 
 
-func _on_DeathArea_body_entered(body):
+func _on_death(body):
 	if body is Player:
 		body.position = _active_screen.spawnpoint + _active_screen.position
 		_grapnel.retract()
--- tiled_import.gd
+++ tiled_import.gd
@@ -7,21 +7,26 @@ const UtilityPole = preload("res://entities/utility_pole/utility_pole.tscn")
 
 func post_import(scene: Node2D):
 	var screen = Screen.instance()
-	var size = Vector2.ZERO
+	var size = null
 
 	for node in scene.get_children():
 		if node is TileMap:
-			for tile in node.get_used_cells():
-				if tile.x > size.x:
-					size.x = tile.x
-				if tile.y > size.y:
-					size.y = tile.y
-			size = (Vector2(1, 1) + size) * 8
+			if size == null:
+				size = Vector2.ZERO
+				for tile in node.get_used_cells():
+					if tile.x > size.x:
+						size.x = tile.x
+					if tile.y > size.y:
+						size.y = tile.y
+				size = (Vector2(1, 1) + size) * 8
+			
 			node.position -= size / 2
 			scene.remove_child(node)
 			screen.add_child(node)
 			node.set_owner(screen)
 			add_occlusion(node.tile_set)
+			if node.has_meta("z-index"):
+				node.z_index = node.get_meta("z-index")
 			
 		elif node is Node2D:  # Object layer
 			var object_layer = node
